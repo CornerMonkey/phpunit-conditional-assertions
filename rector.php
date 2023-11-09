fb97ef27b9175f72d526c515cc83ac154b9ed551
Tim Lawson <tim@lawson.fyi>
2023-11-10 08:07:43 +1000

    Fixed up rules and update composer scripts

--- rector.php
+++ rector.php
@@ -19,7 +19,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_80,
         SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
         SetList::TYPE_DECLARATION,
         SetList::PRIVATIZATION,
